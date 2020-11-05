4378cf53c7bb939979347f59751fb374355ba076
Rasceder <rasmus.cederfeldt@gmail.com>
2020-11-05 18:25:39 +0100

    Update Movie.java

--- src/Movie.java
+++ src/Movie.java
@@ -1,12 +1,7 @@
-public class Movie {
+public class Movie extends Item{
 	
-	private String title;
 	private int runtime;
-	private float rating;
-	private int isbn;
-	private double price;
-	
-	
+	private float rating;	
 	
 	public float getRating() {
 		return rating;
@@ -16,55 +11,40 @@ public class Movie {
 		this.rating = rating;
 	}
 
-	public String getTitle() {
-		return title;
-	}
 
 	public int getRuntime() {
 		return runtime;
 	}
-	
-	public int getISBN() {
-		return isbn;
-	}
-	
-	public double getPrice() {
-		return price;
-		
-	}
 
-	public Movie(String title, int runtime, float rating, int isbn, double price) {
-		super();
-		this.title = title;
+	public Movie(int articleNumber, String title, float cost, int runtime, float rating, String status) {
+		super(articleNumber, title, cost, status);
 		this.runtime = runtime;
 		this.rating = rating;
-		this.isbn = isbn;
-		this.price = price;
 	}
 	
-	/* hejhej */
-	
 	public String movieCsvRecord() {
-		return String.format("%s,%d,%.2f,%d,%lf", title, runtime, rating, isbn, price);
+		return String.format("%d,%s,%.2f,%d,%.1f,%s,", articleNumber, title, cost, runtime, rating, status);
 	}
 
 	public static String getCsvHeaderString() {
-		return "title,runtime (minutes),imdb_rating, isbn(articlenumber), price";
+		return "article number,title,cost,runtime,rating,status";
 	}
 
 	public static Movie parseMovie(String csvRecord) {
 		String[] values = csvRecord.split(",");
-		String title = values[0];
-		int runtime = Integer.parseInt(values[1]);
-		float rating = Float.parseFloat(values[2]);
-		int isbn = Integer.parseInt (values[3]);
-		double price = Double.parseDouble (values[4]);
-		return new Movie(title, runtime, rating, isbn, price);
+		int articleNumber = Integer.parseInt(values[0]);
+		String title = values[1];
+		float cost = Float.parseFloat(values[2]);
+		int runtime = Integer.parseInt(values[3]);
+		float rating = Float.parseFloat(values[4]);
+		String status = values[5];
+
+		return new Movie(articleNumber, title, cost, runtime, rating, status);
 	}
 	
 	@Override
 	public String toString() {
-		return String.format("%s: Runtime %dm, Rating %.2f, ISBN %dm, Price %lf", title, runtime, rating, isbn, price);
+		return String.format("Article Number: %d, Title: %s, Price: %.2f, Runtime: %d, Rating: %.1f, Status: %s,", articleNumber, title, cost, runtime, rating, status);
 	}
 	
 }
